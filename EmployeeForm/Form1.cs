e07c3d0a52e47e3c72b75b0ef649a81bf822c5f3
user <user@DESKTOP-2UMPB9T>
2024-07-31 17:25:50 -0400

    Add project files.

---
+++ EmployeeForm/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace EmployeeForm
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection _conn = new SqlConnection(ConfigurationSettings.AppSettings["DBConnection"].ToString());
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
